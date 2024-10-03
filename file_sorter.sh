df557bf653d4d7b344458c857b86fc479d73c83f
rsmythrepo <raphaelle.smyth@gmail.com>
2024-10-03 12:43:06 +0000

    file with tasks

---
+++ file_sorter.sh
@@ -0,0 +1,21 @@
+# File Sorter 
+
+# input: 50 files, 10 
+
+# function 1 - David
+# Return the file types in a folder > filetypes.txt (unique)
+# output: txt file 
+
+# function 2 - Anwar                                                                                        # Make the folders based on file type (if it does not exist already)
+
+
+# function 3 - Raphaelle
+# Sort files by extension into the folders
+
+# function 4 - all
+# file system information
+# - "Loading message"
+# - file tree diagram (recursive algorithm)
+# - information on folders: size, amount of files (table view)
+# - "Your files have been sorted"
+
