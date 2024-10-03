e53004e28743e9700426b6a07d0529ade27c6749
szabodavid27 <d.szabo85@gmail.com>
2024-10-03 23:14:43 +0000

    listing no_extension

--- extension_extract.sh
+++ extension_extract.sh
@@ -1,26 +1,31 @@
 ext_extract () {
-	src_ext_arr=($(find $1 -type f -exec basename {} \; | grep "\." |sed 's/.*\.//' | tr "\n" " "))
-	src_arr_red=()
-	length=${#src_ext_arr[@]}
+	extensions=($(find $1 -type f -exec basename {} \; | grep "\." |sed 's/.*\.//' | tr "\n" " "))
+	extensions_red=()
+	length=${#extensions[@]}
 
 	for ((i=0; i<length; i++));
 	do
 		duplicate=0
-		length_reduced=${#src_arr_red[@]}
+		length_reduced=${#extensions_red[@]}
 		for ((j=0; j<length_reduced; j++));
 		do
-			if [[ ${src_ext_arr[i]} =~ ${src_arr_red[j]} ]]; then
+			if [[ ${extensions[i]} =~ ${extensions_red[j]} ]]; then
 				duplicate=1
 				break
 			fi
 		done
 		
 		if ((duplicate==0)); then
-			src_arr_red+=(${src_ext_arr[i]})
+			extensions_red+=(${extensions[i]})
 		fi
 	done
-
-	echo "${src_arr_red[@]}"
+	
+	no_extension=($(find $1 -type f -exec basename {} \; | grep -v "\." | tr "\n" " "))
+	if (( ${#no_extension[@]} > 0 )); then
+		extensions_red+=("no_extension")
+	fi
+	
+	echo "${extensions_red[@]}"
 }
 
 ext_extract $1
